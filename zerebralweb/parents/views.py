ceafaacad0356c12ecb8f21d1ccf656420e2abb2
Eric Barch <ericb@ericbarch.com>
2013-08-07 15:17:43 -0400

    Consent layout

--- zerebralweb/parents/views.py
+++ zerebralweb/parents/views.py
@@ -6,10 +6,12 @@ from auth.helpers import get_django_user_for_student
 
 
 def consent(request, consent_token):
+    student = Student.objects.get(parent_token=consent_token)
+    d_user = get_django_user_for_student(student)
+    student_full_name = d_user.first_name + ' ' + d_user.last_name
+
     if request.method == 'POST':
         if request.POST['digital_signature'] is not None and request.POST['digital_signature'] != '':
-            student = Student.objects.get(parent_token=consent_token)
-
             if student is not None:
                 student.consent_from_parent = True
                 student.consent_signed_by = request.POST['digital_signature']
@@ -19,7 +21,6 @@ def consent(request, consent_token):
 
                 # make the student active if they've already accepted the TOS
                 if zu.tos_accepted is True:
-                    d_user = get_django_user_for_student(student)
                     consent_perm = Permission.objects.get(codename='needs_consent')
                     d_user.user_permissions.remove(consent_perm)
                     student_perm = Permission.objects.get(codename='is_student')
@@ -27,4 +28,4 @@ def consent(request, consent_token):
                     d_user.save()
 
 
-    return render(request, 'parents/consent.html')
+    return render(request, 'parents/consent.html', {'student_name': student_full_name})
